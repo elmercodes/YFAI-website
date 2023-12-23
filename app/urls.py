6cc75832d51af7df62cf385173fc32347fdc7337
Elmer Flores <elmflor12@yahoo.com>
2023-12-23 11:30:14 -0600

    Add dashboard

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="app-index")
+    path('', views.index, name="index"),
+    
+    path('register', views.register, name="register"),
+    
+    path('my-login', views.my_login, name="my-login"),
+    
+    path('dashboard', views.dashboard, name="dashboard"),
 ]
