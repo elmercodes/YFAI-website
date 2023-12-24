d184515f257a4e7e946ac25b291eb5b1ca07f5e2
Elmer Flores <elmflor12@yahoo.com>
2023-12-23 18:06:06 -0600

    Add authentication

--- app/urls.py
+++ app/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('my-login', views.my_login, name="my-login"),
     
     path('dashboard', views.dashboard, name="dashboard"),
+    
+    path('privacy-policy', views.privacy_policy, name="privacy-policy"),
+    
+    path('terms-of-use', views.terms_of_use, name="terms-of-use"),
+    
+    path('user-logout', views.user_logout, name="user-logout")
 ]
--- app/views.py
+++ app/views.py
@@ -1,15 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-# Create your views here.
+from . forms import CreateUserForm, LoginForm
+
+from django.contrib.auth.models import auth
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     return render(request, "index.html")
 
 def register(request):
-    pass
+    
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+    
+    context = {'registerform':form}
+    
+    return render(request, "register.html", context=context)
+
 
 def my_login(request):
-    pass
+    
+    form = LoginForm
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                auth.login(request, user)
+                return redirect("dashboard")
+    
+    context = {'loginform': form}        
+    
+    return render(request, "my-login.html", context=context)
+
+
+@login_required(login_url="my-login")
 def dashboard(request):
-    pass
+    return render(request, "dashboard.html")
+
+def privacy_policy(request):
+    return render (request, "privacy-policy.html")
+
+def terms_of_use(request):
+    return render (request, "terms-of-use.html")
+
+def user_logout(request):
+    auth.logout(request)
+    return redirect("index")
